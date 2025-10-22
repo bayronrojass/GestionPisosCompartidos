e6b96ad2a8e1d73a1a5100c4d291e4151914c3b0
aperpal1 <aperpal1@upv.edu.es>
2025-10-22 10:13:40 +0200

    Lógica de creación de piso

--- app/src/main/java/com/example/gestionpisoscompartidos/ui/piso/crearPiso/CrearPisoViewModel.kt
+++ app/src/main/java/com/example/gestionpisoscompartidos/ui/piso/crearPiso/CrearPisoViewModel.kt
@@ -1,29 +1,45 @@
 package com.example.gestionpisoscompartidos.ui.piso.crearPiso
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.example.gestionpisoscompartidos.data.repository.repositories.RepositoryPiso
 import com.example.gestionpisoscompartidos.data.remote.NetworkModule
-import com.example.gestionpisoscompartidos.data.repository.repositories.Repository
 import com.example.gestionpisoscompartidos.model.PisoRequest
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class CrearPisoViewModel : ViewModel() {
-    private val repository = Repository(NetworkModule.databaseApiService)
+    private val repository = RepositoryPiso(NetworkModule.flatApiService)
 
-    private val _estado = MutableStateFlow<String>("")
-    val estado = _estado.asStateFlow()
+    private val _createFlatResult = MutableStateFlow<Boolean?>(null)
+    val createFlatResult: StateFlow<Boolean?> = _createFlatResult
 
-    fun crearPiso(nombre: String, descripcion: String?, direccion: String?) {
-        viewModelScope.launch {
-            try {
-                val request = PisoRequest(nombre, descripcion, direccion)
-                val response = repository.crearPiso(request)
-                _estado.value = "✅ Piso creado: ${response.nombre}"
-            } catch (e: Exception) {
-                _estado.value = "❌ Error: ${e.message}"
-            }
+    fun nameNull(s: String): Boolean = s.trim().isEmpty()
+
+    fun buttonConditions(
+        apartmentName: String,
+        pickedPhoto: Uri?,
+    ): Boolean = !nameNull(apartmentName) && pickedPhoto != null
+
+    suspend fun createFlat(
+        name: String,
+        description: String,
+        pickedPhoto: Uri,
+    ): Boolean =
+        try {
+            val JavaUri = java.net.URI(pickedPhoto.toString())
+            val pisoRequest =
+                PisoRequest(
+                    nombre = name,
+                    descripcion = description,
+                    fotoURI = JavaUri.toString(),
+                )
+            val response = repository.crearPiso(pisoRequest)
+            _createFlatResult.value = true
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            _createFlatResult.value = false
+            false
         }
-    }
 }
