07ded4d64e34720ca6c106d7568e90e5bf937bbb
bayronrojass <bayronrojas30@gmail.com>
2025-12-17 21:53:57 +0100

    Interfaz Elementos de la lista actualizada

--- app/src/main/java/es/mirumi/es/ui/pizarra/postits/DraggablePostItExpanded.kt
+++ app/src/main/java/es/mirumi/es/ui/pizarra/postits/DraggablePostItExpanded.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
